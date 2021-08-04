16135dabe87c96cf1f1d3668412182c9cde5bc02
Alina <agutierrez72@ucmerced.edu>
2021-08-04 14:50:14 -0700

    create Phrase Component

---
+++ src/components/Phrase.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="phrase">
+
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        guessPhrase: '',
+        unknownPhrase: ''
+    },
+    setup(){
+        guessPhrase = "snowman";
+        unknownPhrase = "________";
+        console.log(guessPhrase.length)
+        console.log(unknownPhrase.length);
+
+        return {guessPhrase, unknownPhrase};
+    }
+
+}
+</script>
+
+<style>
+
+</style>
