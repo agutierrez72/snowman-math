7b29776d9db12577234490a8ee9409cc586e1641
Alina <agutierrez72@ucmerced.edu>
2021-08-05 10:35:56 -0700

    Create Snowman Component

---
+++ src/components/Snowman.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="snowman">
+        <img src="@/assets/snowman1.png">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    img{
+        max-height: 200px;
+    }
+    /* .snowman {
+        @apply grid-flow-col justify-between;
+    } */
+</style>
