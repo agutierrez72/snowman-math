c0d8174708f04ced27d226a0c77996603a032457
Alina <agutierrez72@ucmerced.edu>
2021-08-05 10:35:29 -0700

    Create Game Component to store Problem and Snowman Components

---
+++ src/views/Game.vue
@@ -0,0 +1,27 @@
+<template>
+  <Phrase />
+  <div class="game">
+      <Snowman />
+      <Problem />
+  </div>
+</template>
+
+<script>
+import Phrase from '@/components/Phrase.vue'
+import Snowman from '@/components/Snowman.vue'
+import Problem from '@/components/Problem.vue'
+
+export default {
+    components: { Phrase, Snowman, Problem },
+}
+</script>
+
+<style>
+    .game {
+        /* @apply grid justify-center min-w-max m-4; */
+        @apply flex justify-between p-4;
+    }
+    /* .game-component {
+        @apply flex justify-between;
+    } */
+</style>
