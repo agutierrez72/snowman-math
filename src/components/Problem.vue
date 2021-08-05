1312e3cea054eb19b5f44dae6e3afc56e48de8a9
Alina <agutierrez72@ucmerced.edu>
2021-08-05 10:35:41 -0700

    Create Problem Component

---
+++ src/components/Problem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="problem">
+    <div class="top"><span id="top">23</span></div>
+    <div class="top"><span id="operator">+</span><span id="bottom">34</span></div>
+    <div><input type="text" maxlength="5"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .problem {
+        @apply grid justify-center;
+    }
+     .top {
+        @apply flex justify-end;
+    } 
+    .problem input {
+        @apply rounded-b border-t-4 border-black p-4 bg-gray-50 text-lg text-right focus:outline-none;
+    }
+</style>
